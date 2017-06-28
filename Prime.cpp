b8df6e41003f53ea9a8134f00109398fcfd9c5c2
olphe <olphe@github.com>
2017-06-29 08:12:51 +0900

    a

---
+++ Prime.cpp
@@ -0,0 +1,24 @@
+list<long long int> Prime(long long int M) {
+	list<long long int>P;
+	P.push_back(2);
+	P.push_back(3);
+	for (int i = 5; i <= M; i += 6) {
+		bool flag = true;
+		for (auto j : P) {
+			if (i%j == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag)P.push_back(i);
+		flag = true;
+		for (auto j : P) {
+			if ((i + 2) % j == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag)P.push_back(i + 2);
+	}
+	return P;
+}
---
+++ segment_tree.cpp
@@ -0,0 +1,81 @@
+class Segment_Tree {
+	vector<long long int>v;
+	vector<int>l;
+	vector<int>r;
+	long long int ret;
+	bool is_min;
+	long long int Update(int place) {
+		if (place >= v.size() / 2) {
+			return v[place];
+		}
+		if (is_min) {
+			v[place] = min(Update(place * 2), Update(place * 2 + 1));
+			return v[place];
+		}
+		v[place] = max(Update(place * 2), Update(place * 2 + 1));
+		return v[place];
+	}
+	void RMQ(int a, int b, int place) {
+		if (l[place] >= a&&r[place] <= b) {
+			if (is_min)ret = min(ret, v[place]);
+			else ret = max(ret, v[place]);
+			return;
+		}
+		if (l[place]>b || r[place]<a) return;
+		RMQ(a, b, place * 2);
+		RMQ(a, b, place * 2 + 1);
+		return;
+	}
+public:
+	void Left(int place) {
+		if (place >= v.size() / 2) {
+			l[place] = place - v.size() / 2;
+			return;
+		}
+		Left(place * 2);
+		Left(place * 2 + 1);
+		l[place] = l[place * 2];
+		return;
+	}
+	void Right(int place) {
+		if (place >= v.size() / 2) {
+			r[place] = place - v.size() / 2;
+			return;
+		}
+		Right(place * 2);
+		Right(place * 2 + 1);
+		r[place] = r[place * 2 + 1];
+		return;
+	}
+	Segment_Tree(int num, bool min) {
+		l.resize(num);
+		r.resize(num);
+		if (min) {
+			v.resize(num, LLONG_MAX);
+			is_min = true;
+		}
+		else v.resize(num, LLONG_MIN);
+		Left(1);
+		Right(1);
+	}
+	void Insert(int place, long long int num, bool update) {
+		place += v.size() / 2;
+		v[place] = num;
+		if (!update)return;
+		place /= 2;
+		while (place) {
+			if (is_min)v[place] = min(v[place * 2], v[place * 2 + 1]);
+			else v[place] = max(v[place * 2], v[place * 2 + 1]);
+			place /= 2;
+		}
+	}
+	void TopDown() {
+		Update(1);
+	}
+	long long int RMQ(int a, int b) {
+		if (is_min)ret = LLONG_MAX;
+		else ret = LLONG_MIN;
+		RMQ(a, b, 1);
+		return ret;
+	}
+};
