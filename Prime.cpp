9255c106871664c32893d398437f5d420c34c935
olphe <olphe@github.com>
2017-07-14 22:06:18 +0900

    change prime
    number change

--- Prime.cpp
+++ Prime.cpp
@@ -1,8 +1,8 @@
-list<long long int> Prime(long long int M) {
+list<long long int> Prime(long long int num) {
 	list<long long int>P;
 	P.push_back(2);
 	P.push_back(3);
-	for (int i = 5; i <= M; i += 6) {
+	for (int i = 5; i <= num; i += 6) {
 		bool flag = true;
 		for (auto j : P) {
 			if (i%j == 0) {
