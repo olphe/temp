b5eb382b7ebc4d8c19e6ffc5887a833070824b0a
olphe <olphe@github.com>
2017-08-05 23:35:05 +0900

    power
    It's power power power power power power power power power power power power power power power power power power power power power power power power power power

---
+++ power.cpp
@@ -0,0 +1,10 @@
+long long int power(long long int x, long long int n, long long int M) {
+	long long int tmp = 1;
+
+	if (n > 0) {
+		tmp = power(x, n / 2, M);
+		if (n % 2 == 0) tmp = (tmp*tmp) % M;
+		else tmp = (((tmp*tmp) % M)*x) % M;
+	}
+	return tmp;
+}
