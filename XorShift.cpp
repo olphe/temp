08ec4641d1200e93e40d8d810ac975c6a0c6d37a
olphe <olphe@github.com>
2017-09-06 03:23:35 +0900

    XorShift
    XorShift

---
+++ XorShift.cpp
@@ -0,0 +1,19 @@
+class XorShift {
+	unsigned int x, y, z, w, t;
+public:
+	XorShift() {
+		x = 133553533;
+		y = 314867339;
+		z = 664298413;
+		w = 999999937;
+		t = 0;
+	}
+	unsigned int rand() {
+		t = x ^ (x << 11);
+		x = y;
+		y = z;
+		z = w;
+		w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+		return w & 0x7fffffff;
+	}
+};
