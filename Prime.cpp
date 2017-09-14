2e42552956df70fa0410ae212a0ef2a2a49a28a9
olphe <olphe@github.com>
2017-09-14 22:56:17 +0900

    fix
    fix

--- Prime.cpp
+++ Prime.cpp
@@ -1,10 +1,13 @@
-list<long long int> Prime(long long int num) {
-	list<long long int>P;
+list<int> Prime(int num) {
+	list<int>P;
 	P.push_back(2);
 	P.push_back(3);
 	for (int i = 5; i <= num; i += 6) {
 		bool flag = true;
 		for (auto j : P) {
+			if (j*j > i) {
+				break;
+			}
 			if (i%j == 0) {
 				flag = false;
 				break;
@@ -13,6 +16,9 @@ list<long long int> Prime(long long int num) {
 		if (flag)P.push_back(i);
 		flag = true;
 		for (auto j : P) {
+			if (j*j > i + 2) {
+				break;
+			}
 			if ((i + 2) % j == 0) {
 				flag = false;
 				break;
