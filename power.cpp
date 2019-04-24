0f9bbc1f39dadea4c23e7ba324b707ffd38c5059
olphe <yuki0523_mypc@yahoo.co.jp>
2019-04-25 05:47:05 +0900

    Update power.cpp
    recur->while

--- power.cpp
+++ power.cpp
@@ -1,10 +1,13 @@
 long long int power(long long int x, long long int n, long long int M) {
-	long long int tmp = 1;
-
-	if (n > 0) {
-		tmp = power(x, n / 2, M);
-		if (n % 2 == 0) tmp = (tmp*tmp) % M;
-		else tmp = (((tmp*tmp) % M)*x) % M;
+	long long int ret = 1;
+	long long int by = x;
+	while (n) {
+		if (n & 1) {
+			ret *= by;
+			ret %= M;
+		}
+		by *= by;
+		by %= M;
+		n >>= 1;
 	}
-	return tmp;
 }
