20af79a283a8e3386807ed99a478fd8f9c84d591
olphe <yuki0523_mypc@yahoo.co.jp>
2020-03-27 02:44:28 +0900

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,30 +1,26 @@
-list<int> Prime(int num) {
-	list<int>P;
+vector<int> Prime(int num) {
+	vector<int>ret;
+	ret.push_back(2);
+	ret.push_back(3);
 	for (int i = 5; i <= num; i += 6) {
 		bool flag = true;
-		for (auto j : P) {
-			if (j*j > i) {
-				break;
-			}
+		for (int j = 2; j < ret.size(); j++) {
+			if (ret[j] * ret[j] > i)break;
 			if (i%j == 0) {
 				flag = false;
 				break;
 			}
 		}
-		if (flag)P.push_back(i);
+		if (flag)ret.push_back(i);
 		flag = true;
-		for (auto j : P) {
-			if (j*j > i + 2) {
-				break;
-			}
+		for (int j = 2; j < ret.size(); j++) {
+			if (ret[j] * ret[j] > i + 2)break;
 			if ((i + 2) % j == 0) {
 				flag = false;
 				break;
 			}
 		}
-		if (flag)P.push_back(i + 2);
+		if (flag)ret.push_back(i + 2);
 	}
-	P.push_front(3);
-	P.push_front(2);
-	return P;
+	return ret;
 }
