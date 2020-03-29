a5e70560ae308a572ed585cb46a8a421bed7b879
olphe <yuki0523_mypc@yahoo.co.jp>
2020-03-30 03:12:02 +0900

    Update Euclidean_space.cpp

--- Euclidean_space.cpp
+++ Euclidean_space.cpp
@@ -1,5 +1,3 @@
-const long double EPS = 1e-8;
-
 class Coordinate {
 public:
 	long double x;
@@ -26,6 +24,10 @@ long double cross(Coordinate a, Coordinate b) {
 	return a.x*b.y - b.x*a.y;
 }
 
+long double Distance(Coordinate a, Coordinate b) {
+	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+}
+
 bool LineCross(Coordinate a1, Coordinate a2, Coordinate b1, Coordinate b2) {
-	return cross(a2 - a1, b1 - a1)*cross(a2 - a1, b2 - a1) < EPS&&cross(b2 - b1, a1 - b1)*cross(b2 - b1, a2 - b1) < EPS;
+	return cross(a2 - a1, b1 - a1)*cross(a2 - a1, b2 - a1) < EPS&&cross(b2 - b1, a1 - b1)*cross(b2 - b1, a2 - b1) < EPS && !((a1 - a2).x*(a1 - b1).y == (a1 - a2).y*(a1 - b1).x && (a1 - a2).x*(a1 - b2).y == (a1 - a2).y*(a1 - b2).x&&abs(Distance(a1, a2) - Distance(a1, b1) - Distance(a2, b1)) > EPS&&abs(Distance(a1, a2) - Distance(a2, b2) - Distance(a1, b2)) > EPS&&abs(Distance(b1, b2) - Distance(a1, b1) - Distance(a1, b2)) > EPS&&abs(Distance(b1, b2) - Distance(a2, b1) - Distance(a2, b2)) > EPS);
 }
