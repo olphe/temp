172d236df550ae0e63416b799ab4a5665298f39a
olphe <yuki0523_mypc@yahoo.co.jp>
2021-11-20 22:53:27 +0900

    Update Power.cpp
    オーバーフローするのを修正

--- Power.cpp
+++ Power.cpp
@@ -1,6 +1,6 @@
 long long int power(long long int x, long long int n, long long int M) {
 	long long int ret = 1;
-	long long int by = x;
+	long long int by = x % M;
 	while (n) {
 		if (n & 1) {
 			ret *= by;
